e60c8c87591ff6cdfb53bd087a83cae769ace0fd
Kamil <uhkamil@gmail.com>
2025-11-27 14:43:46 +0100

    Started working on task 2 labs 5

--- Lab5/src/Ex2/JAVA_05_02.java
+++ Lab5/src/Ex2/JAVA_05_02.java
@@ -1,26 +1,50 @@
 import static term.term.*;
 
 static class Ball {
-    private int x, y;
-    private int dx, dy;
+    public int x, y;
+    public int dx, dy;
+    public boolean isBallMoving;
+    public int ball_step = 0;
+    private final int BALL_SPEED = 4;
 
-    public Ball(int boardWidth, int boardHeight) {
+    public Ball(int boardWidth, int boardHeight, int dx, int dy, boolean isBallMoving) {
         this.x = boardWidth / 2;
         this.y = boardHeight / 2;
+        
+        this.dx = 0;
+        this.dy = 0;
+        this.isBallMoving = false;
     }
     
 }
 
 static class Paddle {
-    private int length;
+    private final int length;
+    public int x, y;
+    public int leftPaddleMove = 0;
+    public int rightPaddleMove = 0;
     
-    public Paddle() {
+    public Paddle(int length, int paddleX, int paddleY) {
+        this.length = length;
+        this.x = paddleX;
+        this.y = paddleY;
         
     }
 }
 
+static class Frame {
+    static final int TERMINAL_WIDTH = 120;
+    static final int TERMINAL_HEIGHT = 30;
+    static final int PADDLE_HEIGHT = 7;
+    static final int PADDLE_WIDTH = 3;
+    static final int PADDLE_MARGIN_X = (int) (TERMINAL_WIDTH * 0.2);
+}
+
 static class GameModel {
-    
+    static int scoreLeft = 0;
+    static int scoreRight = 0;
+    static boolean gameRunning = true;
+    static String previousScore = "";
 }
 
 static class GameView {
