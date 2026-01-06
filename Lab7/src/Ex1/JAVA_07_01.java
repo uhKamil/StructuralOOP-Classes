1027200b0219b28e4404cc3c692b4b4ce1d4b5ec
Kamil <uhkamil@gmail.com>
2026-01-06 05:43:45 +0100

    Added tests for task 1 from labs 7

--- Lab7/src/Ex1/JAVA_07_01.java
+++ Lab7/src/Ex1/JAVA_07_01.java
@@ -1,55 +1,58 @@
 // Kamil Wolbach (280161)
+package Ex1;
 import java.util.Scanner;
 
-class LCGNumberGenerator {
-    private long currentSeed;
+class JAVA_07_01 {
+    static class LCGNumberGenerator {
+        protected long currentSeed;
 
-    private static final long LCG_M = 4294967296L;
-    private static final long LCG_A = 214013L;
-    private static final long LCG_C = 2531011L;
+        protected static final long LCG_M = 4294967296L;
+        protected static final long LCG_A = 214013L;
+        protected static final long LCG_C = 2531011L;
 
-    public LCGNumberGenerator(long initialSeed) {
-        this.currentSeed = initialSeed;
-    }
-
-    public long next() {
-        this.currentSeed = (LCG_A * this.currentSeed + LCG_C) % LCG_M;
-        return this.currentSeed;
-    }
-}
-
-void main() {
-    Scanner scanner = new Scanner(System.in);
-
-    final int MIN_A = 10;
-    final int MIN_B = 50;
-    final int SIZE_A = 11;
-
-    while (true) {
-        System.out.print("Enter how many numbers I should generate: ");
-        if (!scanner.hasNextInt()) break;
-        int n = scanner.nextInt();
-        if (n == 0) break;
-
-        System.out.print("Enter the seed: ");
-        long s = scanner.nextLong();
-
-        LCGNumberGenerator generator = new LCGNumberGenerator(s);
-        int sum = 0;
-
-        for (int i = 0; i < n; i++) {
-            long rawValue = generator.next();
-//            int r = (int) (rawValue % 32); // weaker bits
-            int r = (int) (rawValue >> 27); // stronger bits moved to 5 least significant positions
-
-            if (r <= 10) r = r + MIN_A;
-            else r = r + MIN_B - SIZE_A;
-
-            System.out.println(r);
-            sum += r;
+        public LCGNumberGenerator(long initialSeed) {
+            this.currentSeed = initialSeed;
         }
 
-        double average = (double) sum / n;
-        System.out.println("The average of the random number set is " + average);
+        public long next() {
+            this.currentSeed = (LCG_A * this.currentSeed + LCG_C) % LCG_M;
+            return this.currentSeed;
+        }
+    }
+
+    void main() {
+        Scanner scanner = new Scanner(System.in);
+
+        final int MIN_A = 10;
+        final int MIN_B = 50;
+        final int SIZE_A = 11;
+
+        while (true) {
+            System.out.print("Enter how many numbers I should generate: ");
+            if (!scanner.hasNextInt()) break;
+            int n = scanner.nextInt();
+            if (n == 0) break;
+
+            System.out.print("Enter the seed: ");
+            long s = scanner.nextLong();
+
+            LCGNumberGenerator generator = new LCGNumberGenerator(s);
+            int sum = 0;
+
+            for (int i = 0; i < n; i++) {
+                long rawValue = generator.next();
+//            int r = (int) (rawValue % 32); // weaker bits
+                int r = (int) (rawValue >> 27); // stronger bits moved to 5 least significant positions
+
+                if (r <= 10) r = r + MIN_A;
+                else r = r + MIN_B - SIZE_A;
+
+                System.out.println(r);
+                sum += r;
+            }
+
+            double average = (double) sum / n;
+            System.out.println("The average of the random number set is " + average);
+        }
     }
 }
